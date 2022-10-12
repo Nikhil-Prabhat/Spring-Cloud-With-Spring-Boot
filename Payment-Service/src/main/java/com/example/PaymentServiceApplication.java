3008d253355fc330f4af8a9f730ab5c06979e213
Nikhil-Prabhat <62531542+Nikhil-Prabhat@users.noreply.github.com>
2022-10-12 23:50:39 +0530

    Spring Cloud -  Eureka Server and Discovery Client

---
+++ Payment-Service/src/main/java/com/example/PaymentServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+// @EnableEurekaClient - This annotation signifies that it is the client and it needs to be registered with Eureka Server
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+}
